cad95373051da65ac13a71f8e0860698b67f1bc4
Peter Laudel <38426989+PeterLaudel@users.noreply.github.com>
2024-10-06 12:41:43 +0200

    Update page tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
 "use client";
 
+import { FlowbiteRatingAdvancedTheme, Rating } from "flowbite-react";
 import Portfolio from "../components/portfolio";
 import Timeline from "../components/timeline";
+const theme: FlowbiteRatingAdvancedTheme = {
+  base: "grid grid-cols-2 [&>*:nth-child(1)]:col-span-2",
+  label: "text-sm font-medium text-cyan-600 dark:text-cyan-500",
+  progress: {
+    label: "text-sm font-medium text-cyan-600 dark:text-cyan-500",
+    base: "mx-4 h-5 w-2/4 rounded bg-gray-200 dark:bg-gray-700",
+    fill: "h-5 rounded bg-yellow-400",
+  },
+};
 
 export default function Home() {
   return (
@@ -13,6 +23,15 @@ export default function Home() {
       <section id="portfolio">
         <h1>Portfolio</h1>
         <Portfolio />
+      </section>
+
+      <section>
+        <h1>Skills</h1>
+        <div className="grid grid-cols-2 gap-4 justify-start">
+          <Rating.Advanced theme={theme} percentFilled={70}>
+            Test
+          </Rating.Advanced>
+        </div>
       </section>
     </main>
   );
